f31c9203c71e3adee7caf3c46598b08e7da2d75d
tokku5552 <physics.0121@gmail.com>
2021-09-08 01:39:59 +0900

    GooglePhotoAPIでcontentsを検索するところまで

--- src/google_photos.py
+++ src/google_photos.py
@@ -1,10 +1,13 @@
-# google_photos.py
+# -*- coding: utf_8 -*-
+
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
 import json
 import os
+import pprint
 from datetime import datetime, date
-from googleapiclient.discovery import build
+
 
 SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
 API_SERVICE_NAME = 'photoslibrary'
@@ -38,8 +41,48 @@ def getCredentials():
     with open(CREDENTIAL_FILE, mode='w') as f_credential_w:
         f_credential_w.write(json.dumps(
             vars(credentials), default=support_datetime_default, sort_keys=True))
-
     return credentials
+
+
+def getPhotos(service):
+    photos = []
+    now = datetime.now()
+    nextPageTokenMediaItems = ''
+    while True:
+        photo = {}
+        body = {
+            'pageSize': 50,
+            "filters": {
+                "dateFilter": {
+                    "ranges": [
+                        {
+                            "startDate": {
+                                "year": now.year,
+                                "month": now.month - 1,
+                                "day": now.day
+                            },
+                            "endDate": {
+                                "year": now.year,
+                                "month": now.month,
+                                "day": now.day,
+                            },
+                        },
+                    ],
+                },
+            },
+            'pageToken': nextPageTokenMediaItems
+        }
+        mediaItems = service.mediaItems().search(body=body).execute()
+        # pprint.pprint(mediaItems)
+        for mediaItem in mediaItems['mediaItems']:
+            photo['id'], photo['filename'], photo['url'] = mediaItem['id'], mediaItem['filename'], mediaItem['baseUrl']
+            pprint.pprint(mediaItem)
+            photos.append(photo)
+        if 'nextPageToken' in mediaItems:
+            nextPageTokenMediaItems = mediaItems['nextPageToken']
+        else:
+            break
+    pprint.pprint(photos)
 
 
 def main():
@@ -47,8 +90,11 @@ def main():
     service = build(
         API_SERVICE_NAME,
         API_VERSION,
-        credentials=credentials
+        credentials=credentials, static_discovery=False
     )
+    getPhotos(service)
+    # result: dict = service.mediaItems().list(pageSize=1).execute()
+    # print(json.dumps(result, ensure_ascii=False))
 
 
 if __name__ == "__main__":
