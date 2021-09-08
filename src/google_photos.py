d4d02747e37f98f555662f899043f45644968d46
tokku5552 <physics.0121@gmail.com>
2021-09-09 00:14:23 +0900

    主要機能実装

--- src/google_photos.py
+++ src/google_photos.py
@@ -10,15 +10,21 @@ from datetime import datetime
 import urllib.request
 import shutil
 import glob
+import settings
 
-SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
-API_SERVICE_NAME = 'photoslibrary'
-API_VERSION = 'v1'
-CLIENT_SECRET_FILE = 'client_secret.json'
-CREDENTIAL_FILE = 'credential.json'
-AQUIRED_MEDIA_LIST = 'aquired_list.json'
-TMP_DIR = 'tmp'
-DESTINATION_DIR = 'gpbk'
+# variable declaration
+SCOPES = settings.SCOPES
+API_SERVICE_NAME = settings.API_SERVICE_NAME
+API_VERSION = settings.API_VERSION
+CLIENT_SECRET_FILE = settings.CLIENT_SECRET_FILE
+CREDENTIAL_FILE = settings.CREDENTIAL_FILE
+AQUIRED_MEDIA_LIST = settings.AQUIRED_MEDIA_LIST
+TMP_DIR = settings.TMP_DIR
+DESTINATION_DIR = settings.DESTINATION_DIR
+QUERY_FILTER = settings.QUERY_FILTER
+PAST_YEARS = settings.PAST_YEARS
+PAST_MONTHS = settings.PAST_MONTHS
+PAST_DAYS = settings.PAST_DAYS
 
 
 def support_datetime_default(o: object) -> None:
@@ -81,37 +87,18 @@ def getMediaIds(service):
 
     Returns
     ----------
-    photos : dict
-    videos : dict
+    photos : list
+        id,filename,url
+    videos : list
+        id,filename,url
     """
     photos = []
     videos = []
     now = datetime.now()
     nextPageTokenMediaItems = ''
     while True:
-        body = {
-            'pageSize': 50,
-            "filters": {
-                "dateFilter": {
-                    "ranges": [
-                        {
-                            "startDate": {
-                                "year": now.year,
-                                "month": now.month - 1,
-                                "day": now.day
-                            },
-                            "endDate": {
-                                "year": now.year,
-                                "month": now.month,
-                                "day": now.day,
-                            },
-                        },
-                    ],
-                },
-            },
-            'pageToken': nextPageTokenMediaItems
-        }
-        mediaItems = service.mediaItems().search(body=body).execute()
+        queryBody = getQueryBody(nextPageTokenMediaItems, now, QUERY_FILTER)
+        mediaItems = service.mediaItems().search(body=queryBody).execute()
         for mediaItem in mediaItems['mediaItems']:
             photo = {}
             video = {}
@@ -131,14 +118,70 @@ def getMediaIds(service):
     return photos, videos
 
 
-def downloadMedia(mediaDict: dict, isVideo: bool) -> list:
+def getQueryBody(nextPageTokenMediaItems, referenceDate, isFilter: bool) -> dict[str, any]:
+    """
+    GooglePhotoAPIに接続するときのquery bodyを生成する
+
+    Returns
+    ----------
+    body : dict[str,any]
+        query body
+    """
+    if not isFilter:
+        body = {
+            'pageSize': 50,
+            'pageToken': nextPageTokenMediaItems
+        }
+        return body
+
+    body = {
+        'pageSize': 50,
+        "filters": {
+            "dateFilter": {
+                "ranges": [
+                    {
+                        "startDate": {
+                            "year": referenceDate.year - PAST_YEARS,
+                            "month": referenceDate.month - PAST_MONTHS,
+                            "day": referenceDate.day - PAST_DAYS
+                        },
+                        "endDate": {
+                            "year": referenceDate.year,
+                            "month": referenceDate.month,
+                            "day": referenceDate.day,
+                        },
+                    },
+                ],
+            },
+        },
+        'pageToken': nextPageTokenMediaItems
+    }
+    return body
+
+
+def downloadMedia(mediaItems: list, isVideo: bool) -> list:
+    """
+    GooglePhotoAPIに接続しphotoもしくはvideoをダウンロードする
+
+    Parameters
+    ----------
+    mediaItems : list
+        メディア(photo or video)のリスト
+    isVideo : bool
+        ビデオであるかどうか
+
+    Returns
+    ----------
+    ids : list
+        ダウンロードに成功したidのリスト
+    """
     suffix: str = '=d'
     if isVideo:
         suffix = '=dv'
     if not os.path.exists(TMP_DIR):
         os.mkdir(TMP_DIR)
     ids = []
-    for item in mediaDict:
+    for item in mediaItems:
         urllib.request.urlretrieve(
             item['url']+suffix, TMP_DIR+'/'+item['filename'])
         ids.append(item['id'])
@@ -146,11 +189,17 @@ def downloadMedia(mediaDict: dict, isVideo: bool) -> list:
 
 
 def toJsonFromIds(ids: list) -> None:
+    """
+    download済みのIdをリストとしてファイルに出力する
+    """
     with open(AQUIRED_MEDIA_LIST, 'w') as f:
         json.dump(ids, f, ensure_ascii=False)
 
 
 def loadIdsJson() -> list:
+    """
+    download済みのIdリストを読み込む
+    """
     result = []
     if os.path.exists(AQUIRED_MEDIA_LIST):
         with open(AQUIRED_MEDIA_LIST) as f_credential_r:
@@ -159,6 +208,16 @@ def loadIdsJson() -> list:
 
 
 def removeAquiredMedia(mediaItems: list, ids: list) -> list:
+    """
+    download済みのitemを除去する
+
+    Parameters
+    ----------
+    mediaItems : list
+        メディア(photo or video)のリスト
+    ids : list
+        download済みのidのリスト
+    """
     result = []
     for item in mediaItems:
         tmp = {}
@@ -169,6 +228,10 @@ def removeAquiredMedia(mediaItems: list, ids: list) -> list:
 
 
 def moveFiles(destinationPath):
+    """
+    TMP_DIRにダウンロードしたファイルを
+    destinationPathにmoveする
+    """
     for p in glob.glob(TMP_DIR+'/*', recursive=False):
         shutil.move(p, destinationPath)
 
@@ -194,7 +257,7 @@ def main():
     videoIds = downloadMedia(videos, True)
 
     # バックアップフォルダへの移動
-    moveFiles('/gpbk')
+    moveFiles(DESTINATION_DIR)
 
     # 取得済みリストの更新
     result = []
@@ -202,12 +265,6 @@ def main():
     result.extend(videoIds)
     result.extend(acquiredIds)
     toJsonFromIds(result)
-
-    # pprint.pprint(photos)
-    # pprint.pprint(videos)
-    # print('photos :'+str(len(photos))+',videos :'+str(len(videos)))
-    # result: dict = service.mediaItems().list(pageSize=1).execute()
-    # print(json.dumps(result, ensure_ascii=False))
 
 
 if __name__ == "__main__":
---
+++ src/settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf_8 -*-
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
+API_SERVICE_NAME = 'photoslibrary'
+API_VERSION = 'v1'
+CLIENT_SECRET_FILE = 'client_secret.json'
+CREDENTIAL_FILE = 'credential.json'
+AQUIRED_MEDIA_LIST = 'aquired_list.json'
+TMP_DIR = 'tmp'
+DESTINATION_DIR = '/gpbk'
+QUERY_FILTER = True
+PAST_YEARS = 0
+PAST_MONTHS = 1
+PAST_DAYS = 0
